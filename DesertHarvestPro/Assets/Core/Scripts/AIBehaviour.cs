07854b2cfbfceeb88cbc0c21b7e85db464659af5
Tomas Moesgaard <tomahawk@os.dk>
2015-10-15 16:15:42 +0200

    Implemented waypoints and added more animations

--- DesertHarvestPro/Assets/Core/Scripts/AIBehaviour.cs
+++ DesertHarvestPro/Assets/Core/Scripts/AIBehaviour.cs
@@ -1,15 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class AIBehaviour : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private AICharacterControl nav;
+
+    private string state;
+    private string subState;
+
+    public GameObject[] Waypoints;
+    private GameObject currentWaypoint;
+    private int waypointIndex;
+
+    // Use this for initialization
+    void Start () {
+
+        nav = GetComponent<AICharacterControl>();
+
+        waypointIndex = 0;
+
+        currentWaypoint = Waypoints[waypointIndex];
+
+        state = "Patrol";
+
+        nav.target = currentWaypoint.transform;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        switch (state)
+        {
+            case "Idle":
+
+                break;
+
+            case "Combat":
+
+                break;
+
+            case "Patrol":
+
+                nav.agent.speed = 0.5f;
+                if (Vector3.Distance(transform.position, currentWaypoint.transform.position) < 2f)
+                {
+                    
+                    waypointIndex++;
+
+                    if (waypointIndex > Waypoints.Length - 1)
+                        waypointIndex = 0;
+
+                    currentWaypoint = Waypoints[waypointIndex];
+                    nav.target = currentWaypoint.transform;
+                }
+
+                
+                break;
+
+        }
 	}
 }
