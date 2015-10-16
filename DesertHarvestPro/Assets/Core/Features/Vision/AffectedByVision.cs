fb25fcfde4a74d887042f56a44aa3388289a0fab
Veselin <veselqshko@gmail.com>
2015-10-16 14:36:40 +0200

    Updated tag manager and added vision scripts

---
+++ DesertHarvestPro/Assets/Core/Features/Vision/AffectedByVision.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AffectedByVision : MonoBehaviour {
+
+	private string tag;
+
+	public Material spiceVisionMat;
+	public Material waterVisionMat;
+	private Material defaultMat;
+	private Shader bufferShader;
+
+	// Use this for initialization
+	void Start () {
+		tag = gameObject.tag;
+
+		if(GetComponent<MeshRenderer>())
+		{
+			defaultMat = GetComponent<MeshRenderer>().material;
+		}
+		else if(GetComponent<SkinnedMeshRenderer>())
+		{
+			defaultMat = GetComponent<SkinnedMeshRenderer>().material;
+		}
+
+		bufferShader = Resources.Load<Shader>("Custom/ItemGlow");
+	}
+
+
+	void OnEnable()
+	{
+		if(tag == "Spice")
+		{
+			ApplySpiceVision();
+		}
+		else if(tag == "Water")
+		{
+			ApplyWaterVision();
+		}
+	}
+
+	void OnDisable()
+	{
+		GetComponent<MeshRenderer>().material = defaultMat;
+	}
+
+	void ApplySpiceVision()
+	{
+		Texture Base = GetComponent<MeshRenderer>().material.GetTexture("_MainTex");
+		Texture NormalMap = GetComponent<MeshRenderer>().material.GetTexture("_BumpMap");
+//		Material spiceMat = new Material();
+		//spiceMat.shader
+		GetComponent<MeshRenderer>().material = spiceVisionMat;
+	}
+
+	void ApplyWaterVision()
+	{
+		GetComponent<MeshRenderer>().material = waterVisionMat;
+	}
+
+	Material CreateMaterial(Color emissionColor)
+	{
+//		Material m = new Material(outlineBufferShader);
+//		m.SetColor("_ColorTint", emissionColor);
+
+		/*
+		m.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+		m.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+		m.SetInt("_ZWrite", 0);
+		m.DisableKeyword("_ALPHATEST_ON");
+		m.EnableKeyword("_ALPHABLEND_ON");
+		m.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+		m.renderQueue = 3000;
+		*/
+//		return m;
+		return null;
+	}
+
+}
---
+++ DesertHarvestPro/Assets/Core/Features/Vision/Vision.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vision : MonoBehaviour {
+
+	public bool isVisionActive = false;
+	private SphereCollider VisionTrigger;
+	// Use this for initialization
+	void Start () {
+		VisionTrigger = GetComponent<SphereCollider>();
+	}
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			ActivateVision();
+		}
+		if(Input.GetKeyUp(KeyCode.E))
+		{
+			DeactivateVision();
+		}
+	}
+	
+	void ActivateVision()
+	{
+		isVisionActive = true;
+		VisionTrigger.enabled = true;
+		//Debug.Log("Vision " +  isVisionActive);
+		
+	}
+	
+	void DeactivateVision()
+	{
+		isVisionActive = false;
+		VisionTrigger.enabled = false;
+		//Debug.Log("Vision " +  isVisionActive);
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.layer == LayerMask.NameToLayer("Vision") && isVisionActive )
+		{
+			Debug.Log("Vision" + col.gameObject.name);
+			col.GetComponent<AffectedByVision>().enabled = true;
+			/*
+			if(col.tag == "Spice")
+			{
+				//col.GetComponent<AffectedByVision>()
+			}
+			else if(col.tag == "Water")
+			{
+
+			}
+			*/
+		}
+	}
+
+	
+	void OnTriggerExit(Collider col)
+	{
+		if(col.gameObject.layer == LayerMask.NameToLayer("Vision") && isVisionActive )
+		{
+			Debug.Log("Exit" + col.gameObject.name);
+			col.GetComponent<AffectedByVision>().enabled = false;
+		}
+	}
+
+}
