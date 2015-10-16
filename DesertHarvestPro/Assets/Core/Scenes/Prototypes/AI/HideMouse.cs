1f21ef63687a137429dae65ca21cf518ff2e2f75
Tomas Moesgaard <tomahawk@os.dk>
2015-10-16 14:30:57 +0200

    Implemented enemy sight

---
+++ DesertHarvestPro/Assets/Core/Scenes/Prototypes/AI/HideMouse.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideMouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Cursor.lockState = CursorLockMode.Locked;
+
+        Cursor.visible = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
