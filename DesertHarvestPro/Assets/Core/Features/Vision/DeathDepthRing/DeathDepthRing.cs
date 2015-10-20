082f7b2e83d73a039e6fe23ee78b76975ee10074
Veselin <veselqshko@gmail.com>
2015-10-20 10:22:28 +0200

    some changes to the shaders and classes

--- DesertHarvestPro/Assets/Core/Features/Vision/DeathDepthRing/DeathDepthRing.cs
+++ DesertHarvestPro/Assets/Core/Features/Vision/DeathDepthRing/DeathDepthRing.cs
@@ -7,64 +7,88 @@ public class DeathDepthRing : MonoBehaviour {
 	public Material mat;
 	public LayerMask everything;
 	public LayerMask nothing;
-
+	public bool enableBlast = false;
+	public bool isLerping = false;
+	private Material tempMat; 
 	void Awake()
 	{
-		mat.SetFloat("_RunRingPass", 0);
-		Debug.Log("Start");
+		tempMat = new Material(mat);
+		tempMat.SetFloat("_RunRingPass", 0);
 	}
 
 	void Start () {
 		GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
 	}
 
-	/*
+
 	void Update (){
 
+
 		if (Input.GetKeyDown(KeyCode.E) || Input.GetMouseButtonDown(1)){
+			//transform.GetChild(0).GetComponent<Camera>().cullingMask = nothing;
+			//GetComponent<Camera>().cullingMask = everything;
 			//set _StartingTime to current time
 			mat.SetFloat("_StartingTime", Time.time);
 			//set _RunRingPass to 1 to start the ring
 			mat.SetFloat("_RunRingPass", 1);
 		}
-		else if (Input.GetKeyUp(KeyCode.E) || Input.GetMouseButtonUp(1)){
-			mat.SetFloat("_StartingTime", Time.time);
-			mat.SetFloat("_RunRingPass", 2);
+
+
+		/*
+		if(enableBlast)
+		{
+			Blast();
+			enableBlast = false;
+			isLerping = true;
 		}
+		if(isLerping)
+		{
+			Material tempMat2 = GetComponent<SineHeatPostProcess>().mat;
+			if(tempMat2.GetFloat("_CloseDepth") > 0.1)
+			{
+				float depth = tempMat2.GetFloat("_CloseDepth") - Time.deltaTime;
+				tempMat2.SetFloat("_CloseDepth", depth);
+			}
+			else
+			{
+				isLerping = false;
+			}
+		}
+		*/
 
 	}
-	*/
 
-	void OnEnable()
+
+	public void Blast()
 	{
 		transform.GetChild(0).GetComponent<Camera>().cullingMask = nothing;
 		GetComponent<Camera>().cullingMask = everything;
-		Debug.Log("OnEnable deathring");
+		//Debug.Log("OnEnable deathring");
 		//set _StartingTime to current time
-		mat.SetFloat("_StartingTime", Time.time);
+		tempMat.SetFloat("_StartingTime", Time.time);
 		//set _RunRingPass to 1 to start the ring
-		mat.SetFloat("_RunRingPass", 1);
-		StartCoroutine(LerpSineToFront());
+		tempMat.SetFloat("_RunRingPass", 1);
+		//StartCoroutine(LerpSineToFront());
 
 
 	}
 
 	IEnumerator LerpSineToFront()
 	{
-		Material tempMat = GetComponent<SineHeatPostProcess>().mat;
-		while(tempMat.GetFloat("_CloseDepth") > 0.1)
+		Material tempMat2 = GetComponent<SineHeatPostProcess>().mat;
+		while(tempMat2.GetFloat("_CloseDepth") > 0.1)
 		{
-			float depth = tempMat.GetFloat("_CloseDepth") - Time.deltaTime;
-			tempMat.SetFloat("_CloseDepth", depth);
+			float depth = tempMat2.GetFloat("_CloseDepth") - Time.deltaTime;
+			tempMat2.SetFloat("_CloseDepth", depth);
 			yield return null;
 		}
-		tempMat.SetFloat("_CloseDepth", 0);
+		tempMat2.SetFloat("_CloseDepth", 0);
 		yield return null;
 	}
 	
 	// Called by the camera to apply the image effect
 	void OnRenderImage (RenderTexture source, RenderTexture destination){
 		//mat is the material containing your shader
-		Graphics.Blit(source,destination,mat);
+		Graphics.Blit(source,destination,tempMat);
 	}
 }
