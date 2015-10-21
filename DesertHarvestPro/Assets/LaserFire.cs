80db754848ba17d1f15b8fa8f5c4b0a3007ad9d5
Tomas Moesgaard <tomahawk@os.dk>
2015-10-21 13:49:32 +0200

    Added laser

---
+++ DesertHarvestPro/Assets/LaserFire.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserFire : MonoBehaviour {
+
+    private LineRenderer linerenderer;
+
+    public GameObject barrel;
+
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+        linerenderer = GetComponent<LineRenderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ShootCall(GameObject p)
+    {
+        StartCoroutine(Shoot(p));
+    }
+
+
+    IEnumerator Shoot(GameObject p)
+    {
+        linerenderer.SetPosition(0, barrel.transform.position);
+
+        linerenderer.SetPosition(1, p.transform.position);
+
+        yield return new WaitForSeconds(0.3f);
+
+    }
+}
